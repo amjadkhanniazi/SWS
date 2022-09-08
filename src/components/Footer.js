7e61c23aa179aa015d4dd4bed7815e9b9cc13ae9
Amjad-Khan-360 <2019cs45@student.uet.edu.pk>
2022-09-08 21:07:10 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -112,7 +112,7 @@ export default function Footer() {
         <section>
         <div class="text-center p-4 border-top" style={{color:"white"}}>
     © 2021 Copyright:
-    <Link class="text-reset fw-bold" to="https://mdbootstrap.com/">SocialWelfareSystem.co</Link>
+    <Link class="text-reset fw-bold" to="https://mdbootstrap.com/">SocialWelfareSystem.com</Link>
   </div>
         </section>    
   </footer>
